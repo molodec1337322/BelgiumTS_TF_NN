e7d55aa51b08817e016639807ea26e3b86b905dd
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-12-20 15:28:38 +0300

    Add files via upload

--- run_model.py
+++ run_model.py
@@ -1,11 +1,22 @@
 import research_datasets as rd
 from tensorflow import keras
+from keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
-
-images, labels = rd.get_train_data()
-test_images, test_labels = rd.get_test_data()
 
 model = keras.models.load_model('model.h5')
 
-predictions = model.predict(test_images)
-print(predictions)
+img_path = os.path.join(os.getcwd(), 'Testing', '00000', '00017_00000.ppm')
+img = image.load_img(img_path, target_size=(28, 28), grayscale=True)
+plt.imshow(img, cmap='gray')
+plt.show()
+
+x = image.img_to_array(img)
+x = 255 - x
+x /= 255
+x = x.reshape(1, 28, 28)
+
+predictions = model.predict(x)
+
