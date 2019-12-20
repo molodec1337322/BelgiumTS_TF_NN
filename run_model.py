67a775b8b560fab090f21cabaeeed424db8ee65a
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-12-21 00:39:17 +0300

    Update run_model.py

--- run_model.py
+++ run_model.py
@@ -8,15 +8,16 @@ import os
 
 model = keras.models.load_model('model.h5')
 
-img_path = os.path.join(os.getcwd(), 'Testing', '00000', '00017_00000.ppm')
-img = image.load_img(img_path, target_size=(28, 28), grayscale=True)
-plt.imshow(img, cmap='gray')
+test_images, test_labels = rd.get_test_data()
+plt.imshow(test_images[0], cmap='gray')
 plt.show()
 
-x = image.img_to_array(img)
+x = image.img_to_array(test_images[0])
 x = 255 - x
 x /= 255
 x = x.reshape(1, 28, 28)
 
-predictions = model.predict(x)
+prediction = model.predict(x)
+print("Модель говорит, что это: ", test_labels(np.argmax(prediction)),
+      "На самом деле это: ", test_labels[0])
 
