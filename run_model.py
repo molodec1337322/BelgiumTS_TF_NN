2f95a4383e520fb2213aaa0adb2a5528099388c8
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-12-28 17:34:35 +0300

    Add files via upload

--- run_model.py
+++ run_model.py
@@ -1,27 +1,59 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import research_datasets as rd
 from tensorflow import keras
-from keras.preprocessing import image
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import random
+
+
+def plot_image(i, predictions_array, true_labels, images):
+
+  predictions_array = predictions_array[i]
+  true_label = true_labels[i]
+  image = images[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(image, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  print('\nМодель говорит что это: ', predicted_label)
+  print('На самом деле это: ', true_label)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{}  {:2.0f}%   ({})".format(predicted_label,
+                                100*np.max(predictions_array),
+                                true_label),
+                                color=color)
+
+
+def show_prediction(index):
+    """ Показывает и визуализирует результат """
+
+    i = index
+    plt.figure(figsize=(6, 3))
+    plt.subplot(1, 2, 1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.show()
 
 
 model = keras.models.load_model('model.h5')
-image_number = 0
 test_images, test_labels = rd.get_test_data()
 
+print(test_labels)
+
+sign_numbers = []
+sign_numbers = [sign_number for sign_number in test_labels if sign_number not in sign_numbers]
+
+predictions = model.predict(test_images)
+
 while(True):
+    show_prediction(random.randint(0, len(test_labels)))
 
-    plt.imshow(test_images[image_number])
-    plt.show()
 
-    x = image.img_to_array(test_images[image_number])
 
-    x = x.reshape(1, 32, 32)
-
-    prediction = model.predict(x)
-    print("Модель говорит, что это: ", test_labels[np.argmax(prediction)],
-          "На самом деле это: ", test_labels[image_number])
-    image_number += 10
 
--- train_model.py
+++ train_model.py
@@ -1,18 +1,47 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
 import research_datasets as rd
 import tensorflow as tf
 from tensorflow import keras
+from keras.models import Model
+from keras.layers import Input, Dense
+from keras.utils import np_utils
+from keras.preprocessing import image
 
 
-images, labels = rd.get_train_data()
+train_images, train_labels = rd.get_train_data()
 test_images, test_labels = rd.get_test_data()
+
+height, width, depth = 28, 28, 1
+train_images_num = len(train_images)
+test_images_num = len(test_images)
+
+print(train_images.shape)
+print(test_images.shape)
+
+
+classes = []
+classes = [sign_number for sign_number in test_labels if sign_number not in classes]
+num_classes = len(classes)
+
+'''
+train_labels = np_utils.to_categorical(train_labels, num_classes)
+test_labels = np_utils.to_categorical(test_labels, num_classes)
+'''
+
 print('Данные подготовлены')
 
+hidden_size = 512
+num_epochs = 80
+batch_size = 128
+
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(32, 32)),
-    keras.layers.Dense(1024, activation=tf.nn.relu),
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(512, activation=tf.nn.relu),
+    keras.layers.Dense(512, activation=tf.nn.relu),
     keras.layers.Dense(62, activation=tf.nn.softmax)
 ])
+
 
 model.compile(
     optimizer='adam',
@@ -20,12 +49,15 @@ model.compile(
     metrics=['accuracy']
 )
 
-model.fit(images, labels, epochs=100)
+model.fit(train_images, train_labels, nb_epoch=num_epochs, verbose=1, validation_split=0.1)
 
 model.save('model.h5')
 print('Модель сохранена!')
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1)
 print('\nТочность на проверочных данных:', test_acc)
+
+
+
 
 
